b421f8dc0cbba0279b71813d8bec1a13a441f495
Bibarys <b.utepov@neocode.kz>
2025-12-08 11:05:51 +0500

    update

--- office_sud_kz/downloadIspListByTalon/parse_links.py
+++ office_sud_kz/downloadIspListByTalon/parse_links.py
@@ -4,13 +4,31 @@ from browser.browser import Browser
 import re
 
 def run(browser: Browser, data):
-    fieldset = browser.driver.find_element(By.CSS_SELECTOR, "fieldset.form-wrapper")
+    try:
+        dinamika = browser.driver.find_element(
+                By.XPATH,
+                "//div[@class='my-cases-folders-item' and .//a[contains(normalize-space(.), 'Динамика хода рассмотрения дела')]]"
+            )
 
-    all_links = fieldset.find_elements(By.TAG_NAME, "a")
+        dinamika_links = dinamika.find_elements(By.TAG_NAME, "a")
+    except Exception:
+        dinamika_links = []
+
+    try:
+        statuses = browser.driver.find_element(
+                By.XPATH,
+                "//div[@class='my-cases-folders-item' and .//a[contains(normalize-space(.), 'Статусы')]]"
+            )
+
+        statuses_links = statuses.find_elements(By.TAG_NAME, "a")
+    except Exception:
+        statuses_links = []
+
+    all_links = dinamika_links + statuses_links
 
     file_links = [
         link for link in all_links
-        if "/attachDownload" in link.get_attribute("href")
+        if "noticeAttachDownload" in link.get_attribute("href")
     ]
 
     print(f"[Worker {data['worker_id']}] Found {len(file_links)} files to download.")
